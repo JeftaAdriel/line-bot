c88b18e97636205dfff478b7261a7adf86a30773
Jefta Adriel <JeftaHeryadi@gmail.com>
2025-01-02 20:25:09 +0700

    Testing

---
+++ api/webhook.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+@app.post("/webhook")
+async def webhook(request: Request):
+    body = await request.json()
+    print("Received event:", body)
+    return {"status": "ok"}
