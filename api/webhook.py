02e6b00dd80a6d6e436e76ca7fe31b66b0977c87
Jefta Adriel <JeftaHeryadi@gmail.com>
2025-01-03 20:45:38 +0700

    Trying signature validation

--- api/webhook.py
+++ api/webhook.py
@@ -2,31 +2,32 @@ import os
 import traceback
 import time
 import logging
+import fastapi
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
-from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
+
+from utils.signature_validation import verify_signature
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 load_dotenv()
 
-app = FastAPI()
-handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
+app = fastapi.FastAPI()
 
 
 @app.post("/webhook")
-async def webhook(request: Request):
-    # Parse the request body as JSON
-    body = await request.json()
+async def webhook(request: fastapi.Request):
+    signature = request.headers.get("X-Line-Signature", "")
+    r_body = await request.body()
+    body_str = r_body.decode("utf-8")
+    try:
+        if not verify_signature(body_str, signature):
+            raise InvalidSignatureError("Invalid signature. signature=" + signature)
+    except InvalidSignatureError as exc:
+        raise fastapi.HTTPException(status_code=400, detail="Invalid signature. Please check your channel access token/channel secret.") from exc
 
-    # Log the received event (incoming JSON)
-    logging.info("Received event: %s", body)
-
-    print("Received event: %s", body)
-
+    print("Received event: %s", r_body)
     print(f"Headers: {request.headers}")
 
-    # Respond to acknowledge the request
-    return {"status": "ok"}
+    return fastapi.responses.JSONResponse(content={"message": "OK"})
--- utils/signature_validation.py
+++ utils/signature_validation.py
@@ -8,9 +8,5 @@ def verify_signature(body, x_line_signature):
     channel_secret = os.environ.get("LINE_CHANNEL_SECRET")
     if not channel_secret:
         raise ValueError("LINE_CHANNEL_SECRET is not set in environment variables.")
-    hash = hmac.new(channel_secret.encode("utf-8"), body.encode("utf-8"), hashlib.sha256).digest()
-    signature = base64.b64encode(hash)
-    if hmac.compare_digest(x_line_signature, signature):
-        return True
-    else:
-        return False
+    gen_signature = hmac.new(channel_secret.encode("utf-8"), body.encode("utf-8"), hashlib.sha256).digest()
+    return hmac.compare_digest(x_line_signature, base64.b64encode(gen_signature))
