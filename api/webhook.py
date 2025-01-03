3c13a8fcc4daa680981d2140582864a37a0daedb
Jefta Adriel <JeftaHeryadi@gmail.com>
2025-01-03 17:40:32 +0700

    Trying again

--- api/webhook.py
+++ api/webhook.py
@@ -1,26 +1,57 @@
-import logging
+import os
 import fastapi
-from utils import signature_validation
+import traceback
+import time
+import logging
+
+from dotenv import load_dotenv
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import Configuration
+from utils.signature_validation import verify_signature
+
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
+
+load_dotenv()
+configuration = Configuration(access_token=os.environ.get("LINE_CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("LINE_CHANNEL_SECRET"))
+
 
 app = fastapi.FastAPI()
 
 
 @app.post("/webhook")
 async def webhook(request: fastapi.Request):
-    headers = await request.headers
-    body = await request.json()
+    signature = request.headers.get("X-Line-Signature", "")
+    r_body = await request.body()
+    body_str = r_body.decode("utf-8")
 
-    is_valid = signature_validation(request)
+    print(f"body_str: {body_str}")
+    print(f"signature: {signature}")
 
-    # Log the received event (incoming JSON)
-    logging.info("Received event: %s", body)
+    try:
+        handler.handle(body_str, signature)
+    except InvalidSignatureError as exc:
+        raise fastapi.HTTPException(status_code=400, detail="Invalid signature. Please check your channel access token/channel secret.") from exc
 
-    print(f"Headers: {headers}")
-    print(f"Received event: {body}")
-    print(f"Is Valid: {is_valid}")
+    is_valid = verify_signature(body_str, signature)
+    print(f"is_valid: {is_valid}")
+
+    # print(f"Headers: {headers}")
+    # print(f"Body: {body}")
+    # body_json = await request.json()
+    # x_line_signature = request.headers.get("x-line-signature")
+
+    # is_valid = verify_signature(body, x_line_signature)
+
+    # # Log the received event (incoming JSON)
+    # logging.info("Received event: %s", body_json)
+
+    # print(f"Headers: {headers}")
+    # print(f"Received event: {body_json}")
+    # print(f"Is Valid: {is_valid}")
 
     # Respond to acknowledge the request
     return {"status": "ok"}
--- utils/signature_validation.py
+++ utils/signature_validation.py
@@ -4,12 +4,13 @@ import hmac
 import os
 
 
-def verify_signature(request):
+def verify_signature(body, x_line_signature):
     channel_secret = os.environ.get("LINE_CHANNEL_SECRET")
-    body = request.body
+    if not channel_secret:
+        raise ValueError("LINE_CHANNEL_SECRET is not set in environment variables.")
     hash = hmac.new(channel_secret.encode("utf-8"), body.encode("utf-8"), hashlib.sha256).digest()
     signature = base64.b64encode(hash)
-    if hmac.compare_digest(request.headers.get["X-Line-Signature"], signature):
+    if hmac.compare_digest(x_line_signature, signature):
         return True
     else:
         return False
